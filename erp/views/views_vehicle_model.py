00a32ca9810ccd04b0111d92eed11cbd7e8edd61
gabrielbrag <gabriel.s.braga2000@gmail.com>
2023-07-16 18:28:07 -0300

    Added vehicle variant creation in model template

--- erp/views/views_vehicle_model.py
+++ erp/views/views_vehicle_model.py
@@ -1,16 +1,18 @@
 from .views_base import DataTableListView
-from django.views.generic import ListView, TemplateView, UpdateView, CreateView
+from django.views.generic import ListView, TemplateView, UpdateView, CreateView, DeleteView
 from django.core.serializers import serialize
 from django.http import JsonResponse
 from erp.models import Brand, Vehicle_model, Vehicle_model_variant
 from django.utils.translation import gettext as _
 from django.urls import reverse, reverse_lazy
+import json
 
 class VehicleModelListView(DataTableListView):
     model = Vehicle_model
     columns = ["brand", "model_name", "model_type"]
     insert_view_route_name = 'vehicle_model_create'
     edit_view_route_name = 'vehicle_model_edit'
+    delete_view_route_name = 'vehicle_model_delete'
 
 class VehicleModelBaseView():
     model = Vehicle_model
@@ -19,10 +21,62 @@ class VehicleModelBaseView():
     success_url = reverse_lazy('vehicle_model_list')
 
 class VehicleModelCreateView(VehicleModelBaseView, CreateView):
-    pass
+    def form_valid(self, form):
+        # Save the main model
+        response = super().form_valid(form)
+
+        variant_data = json.loads(self.request.POST.get('variant_data'))
+        vehicle_model = self.object  # Access the created VehicleModel object
+
+        # Create and save new variants
+        for variant_name in variant_data:
+            variant = Vehicle_model_variant(vehicle_model=vehicle_model, variant_name=variant_name)
+            variant.save()
+
+        return response
+
+        return response
 
 class VehicleModelUpdateView(VehicleModelBaseView, UpdateView):
-    pass
+    removeVariantHTML = '<button class="removeButton"><i class="fas fa-trash"></i></button>'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        variantsList = []
+        variantsQuerySet = Vehicle_model_variant.objects.filter(vehicle_model=self.kwargs.get('pk'))
+        for variant in variantsQuerySet:
+            dataTableDict = {}
+            dataTableDict["variant"]    = variant.variant_name
+            dataTableDict["remove"]     = self.removeVariantHTML
+            variantsList.append(dataTableDict)
+
+        context["removeVariantHTML"] = self.removeVariantHTML
+        context["variants_JSON"] = str(json.dumps(variantsList)).replace("\\", "\\\\")
+        return context
+
+    def form_valid(self, form):
+        # Save the main model
+        response = super().form_valid(form)
+
+        HandleVariants(self)
+
+        return response
+
+class VehicleModelDeleteView(VehicleModelBaseView, DeleteView):
+    def post(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
+def HandleVariants(self):
+    variant_data = json.loads(self.request.POST.get('variant_data'))
+    vehicle_model = self.get_object()  # Retrieve the current VehicleModel object
+
+    # Delete existing variants
+    vehicle_model.variants.all().delete()
+
+    # Create and save new variants
+    for variant_name in variant_data:
+        variant = Vehicle_model_variant(vehicle_model=vehicle_model, variant_name=variant_name)
+        variant.save()
 
 def VehicleModelJSON(request):
     brand_id = request.GET.get('brand')
